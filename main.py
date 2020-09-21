c367caf04cc50b40d11777169b852131eaf5feee
REDxEYE <med45c@gmail.com>
2020-09-21 16:38:23 +0300

    add commit comment warning response on push

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import pycodestyle
 
 from io import StringIO
@@ -7,10 +9,13 @@ from unidiff import PatchSet, PatchedFile, Hunk
 from unidiff.patch import Line
 
 from flask import Flask, request
-from requests import get
+from requests import get, post
 
 app = Flask(__name__)
 app.notifications = {}
+
+GIT_COMMENT_URL = 'https://api.github.com/repos/{owner}/{repo}/commits/{commit_sha}/comments'
+header = {"Authorization": f'Bearer {os.environ.get("GITAPIKEY", "ERROR")}'}
 
 
 def _changed_in_diff(diff: PatchedFile, line_n: int):
@@ -18,7 +23,9 @@ def _changed_in_diff(diff: PatchedFile, line_n: int):
         hunk: Hunk
         for line_change in hunk:
             line_change: Line
-            if line_change.is_added and line_change.target_line_no == line_n:
+            if (line_change.is_added and
+                line_change.target_line_no == line_n) \
+                    or line_change.is_context:
                 return True
     return False
 
@@ -29,7 +36,17 @@ def _get_file_by_name(mod_files: List[PatchedFile], filename: str):
             return mod
     return None
 
-#Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong string
+
+def _comment_on_line(user, repo, commit_sha, line_n, filename, message):
+    res = post(
+        GIT_COMMENT_URL.format(owner=user, repo=repo, commit_sha=commit_sha),
+        headers=header, data={
+            "body": message,
+            "path": filename,
+            "position": line_n
+        })
+    assert res.status_code == 201
+
 
 @app.route('/git_hook', methods=['POST'])
 def git_hook():
@@ -49,13 +66,23 @@ def git_hook():
                 lines=StringIO(raw_content).readlines()
             )
             warning_count = checker.check_all()
-            print(f'REPORT: Total warning count: {warning_count}')
-            for line_n, _, code, text, doc in checker.report._deferred_print:
+            comments_per_line = {}
+            for line_n, offset, code, text, doc in checker.report._deferred_print:
                 if _changed_in_diff(_get_file_by_name(patch_set.modified_files,
                                                       file['filename']),
                                     line_n):
                     print(f'{checker.filename}:{line_n}'
                           f' {code} : {text}')
+                    comments = comments_per_line.get(line_n, [])
+                    comments.append(f'{code} : {text}')
+                    comments_per_line[line_n] = comments
+            for line_n, comments in comments_per_line.items():
+                comment = '\n'.join(comments)
+                _comment_on_line(hook_data['owner']['name'],
+                                 repo_info['name'],
+                                 hook_data['after'],
+                                 line_n,
+                                 file['filename'], comment)
 
     return 'ok'
 
