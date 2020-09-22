24737fb50c1a8f3dc4ac356e2c850cc12cd3b8e8
REDxEYE <med45c@gmail.com>
2020-09-22 16:11:21 +0300

    moved to flake8

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-import pycodestyle
-
-from io import StringIO
+# import pycodestyle
+from flake8.api import legacy as flake8
+from flake8.api.legacy import Report
 
 from unidiff import PatchSet
 
@@ -24,20 +24,19 @@ def handle_push(push_data: dict):
         "Accept": "application/vnd.github.v3.diff"}).content.decode('utf8')
     patch_set = PatchSet(diff_content)
     for file in diff_info['files']:
-        content = get(file['contents_url'],auth=auth).json()
-        raw_content = get(content['download_url']).content.decode('utf8')
-        checker = pycodestyle.Checker(
-            filename=file['filename'],
-            lines=StringIO(raw_content).readlines()
-        )
-        warning_count = checker.check_all()
+        content = get(file['contents_url'], auth=auth).json()
+        raw_content = get(content['download_url']).content
+        with open("flake8_tmp_file.py", 'wb') as f:
+            f.write(raw_content)
+        style_guide = flake8.get_style_guide(ignore=['E24', 'W503'])
+        style_guide.input_file('./flake8_tmp_file.py', )
+        results = style_guide._application.file_checker_manager.checkers[
+            0].results
         comments_per_line = {}
-        for line_n, offset, code, text, doc in checker.report._deferred_print:
+        for code, line_n, offset, text, src in results:
             if _changed_in_diff(_get_file_by_name(patch_set,
                                                   file['filename']),
                                 line_n):
-                # print(f'{checker.filename}:{line_n}'
-                #       f' {code} : {text}')
                 comments = comments_per_line.get(line_n, [])
                 comments.append(f'Line:{line_n}:{offset} -> {code} {text}')
                 comments_per_line[line_n] = comments
@@ -71,4 +70,6 @@ def health():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='9090')
+    with open('tmp.json', 'r') as f:
+        handle_push(json.load(f))
+    # app.run(host='0.0.0.0', port='9090')
