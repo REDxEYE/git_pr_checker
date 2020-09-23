6cf75e8cdd8c09394477768a955306201683b638
REDxEYE <med45c@gmail.com>
2020-09-23 13:04:52 +0300

    fix typo

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 GIT_COMMENT_URL = 'https://{host}/repos/{owner}/{repo}/commits/{commit_sha}/comments'
 GIT_COMPARE_URL = "https://{host}/repos/{owner}/{repo}/compare/{base}...{head}"
-GIT_COMMIT_URL = "https://{host}/repos/{owner}/{repo}/commit/{sha}"
+GIT_COMMIT_URL = "https://{host}/repos/{owner}/{repo}/commits/{sha}"
 GIT_PULL_REVIEW_URL = "https://{host}/repos/{owner}/{repo}/pulls/{pull_number}/reviews"
 GIT_FILE_REF = "https://{host}/{owner}/{repo}/blob/{sha}/{filepath}"
