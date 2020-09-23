208b393c546c5e727d8d3707928cc848ff1ef6d9
REDxEYE <med45c@gmail.com>
2020-09-23 17:59:49 +0300

    major refactor

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
 import json
-
-from flake8.api import legacy as flake8
-
-from unidiff import PatchSet
+import logging
+from logging import Logger
 
 from flask import Flask, request
 from requests import get, post
 
-from api import (GIT_COMMIT_URL,
-                 GIT_FILE_REF,
-                 GIT_PULL_REVIEW_URL,
-                 GIT_COMPARE_URL)
-from utils import (changed_in_diff,
-                   get_file_by_name,
-                   post_comment_on_line,
-                   auth,
-                   host_api, host, format_comment,
-                   )
+from api import GIT_FILE_REF
+
+from utils import (post_comment_on_line, auth, host_api, host,
+                   format_comment, flake8_scan_file, post_pr_review)
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 
 
 def handle_push(push_data: dict):
     repo_info = push_data['repository']
-    results = check_commit(push_data['after'],
-                           repo_info['owner']['login'],
-                           repo_info['name'],
-                           )
+    results = flake8_scan_file(push_data['after'],
+                               repo_info['owner']['login'],
+                               repo_info['name'],
+                               )
     for file_name, comments_per_line in results.items():
         for line_n, comments_data in comments_per_line.items():
             comment = ""
             for comment_data in comments_data:
                 comment += f'\nLine:{comment_data[1]} -> ' \
                            f'{format_comment(*comment_data)}'
-            app.logger.info(
-                'Writing comment on git commit (%s) %s:%i',
-                push_data['after'],
-                file_name,
-                line_n)
+            app.logger.info('Writing comment on git commit (%s) %s:%i',
+                            push_data['after'],
+                            file_name,
+                            line_n)
             post_comment_on_line(repo_info['owner']['login'],
                                  repo_info['name'],
                                  push_data['after'],
@@ -45,92 +37,42 @@ def handle_push(push_data: dict):
                                  file_name, comment)
 
 
-def check_commit(commit_sha, owner, repo, parent_sha=None):
-    if parent_sha is None:
-        link = GIT_COMMIT_URL.format(sha=commit_sha,
-                                     owner=owner,
-                                     repo=repo,
-                                     host=host_api)
-        commit_info_req = get(link, auth=auth)
-        app.logger.critical(f'GET {link}:{commit_info_req.status_code}')
-        commit_info = commit_info_req.json()
-        parent_sha = commit_info['parents'][0]['sha']
-    diff_url = GIT_COMPARE_URL.format(base=parent_sha,
-                                      head=commit_sha,
-                                      owner=owner,
-                                      repo=repo,
-                                      host=host_api)
-    diff_info = get(diff_url, auth=auth).json()
-    diff_content = get(diff_url,
-                       auth=auth,
-                       headers={"Accept": "application/vnd.github.v3.diff"}
-                       ).content.decode('utf8')
-    patch_set = PatchSet(diff_content)
-    comments_per_file = {}
-    for file in diff_info['files']:
-        content = get(file['contents_url'], auth=auth).json()
-        file_content = get(content['download_url']).content
-        with open("flake8_tmp_file.py", 'wb') as test_file:
-            test_file.write(file_content)
-        style_guide = flake8.get_style_guide(ignore=['E24', 'W503'])
-        style_guide.input_file('./flake8_tmp_file.py', )
-        results = style_guide._application.file_checker_manager.checkers[
-            0].results
-        comments_per_line = {}
-        for code, line_n, offset, text, src in results:
-            if changed_in_diff(get_file_by_name(patch_set,
-                                                file['filename']), line_n):
-                comments = comments_per_line.get(line_n, [])
-                comments.append((file['filename'], line_n, offset, code, text))
-                comments_per_line[line_n] = comments
-        comments_per_file[file['filename']] = comments_per_line
-    return comments_per_file
-
-
 def handle_pull_request(hook_data):
     owner = hook_data['repository']['owner']['login']
     repo = hook_data['repository']['name']
     pr_info = hook_data['pull_request']
+    commit_sha = pr_info['head']['sha']
 
     app.logger.info("Checking %s commit against %s commit",
-                    pr_info['head']['sha'],
+                    commit_sha,
                     pr_info['base']['sha'])
 
-    results = check_commit(pr_info['head']['sha'],
-                           owner,
-                           repo,
-                           parent_sha=pr_info['base']['sha'])
+    results = flake8_scan_file(commit_sha,
+                               owner,
+                               repo,
+                               parent_sha=pr_info['base']['sha'])
 
     message = "Dear developer, " \
               "please fix all referenced " \
               "PEP8 code style violations before merging PR."
 
     for filename, comments in results.items():
-        file_ref_template = GIT_FILE_REF.format(host=host,
-                                                owner=owner,
-                                                repo=repo,
-                                                sha=pr_info['head']['sha'],
-                                                filepath=filename)
-        line = f"## [{filename}]({file_ref_template}):"
+        file_ref = GIT_FILE_REF.format(host=host,
+                                       owner=owner,
+                                       repo=repo,
+                                       sha=pr_info['head']['sha'],
+                                       filepath=filename)
+        line = f"## [{filename}]({file_ref}):"
+
         for comments_per_line in comments.values():
             for comment in comments_per_line:
                 line += f'\n[Line {comment[1]}]' \
-                        f'({file_ref_template}#L{comment[1]}):' \
+                        f'({file_ref}#L{comment[1]}):' \
                         f'{format_comment(*comment)}'
         message += "\n" + line
-    review = {
-        "commit_id": pr_info['head']['sha'],
-        "body": message,
-        "event": "COMMENT",
-    }
 
-    res = post(GIT_PULL_REVIEW_URL.format(host=host_api,
-                                          owner=owner,
-                                          repo=repo,
-                                          pull_number=pr_info['number']),
-               json=review, auth=auth)
-    assert res.status_code == 200, f'Got non 201 status, ' \
-                                   f'error message: {res.content}'
+    post_pr_review(owner, repo, commit_sha, pr_info['number'],
+                   message)
 
 
 @app.route('/git_hook', methods=['POST'])
@@ -157,7 +99,4 @@ def health():
 
 
 if __name__ == '__main__':
-    # with open('tmp.json', 'r') as f:
-    #     handle_pull_request(json.load(f))
-    #     handle_push(json.load(f))
     app.run(host='0.0.0.0', port='9090')
--- utils.py
+++ utils.py
@@ -1,12 +1,16 @@
 import os
 from typing import List
 
-from requests.api import post
+from requests.api import post, get
 from requests.auth import HTTPBasicAuth
-from unidiff import PatchedFile, Hunk
+
+from unidiff import PatchedFile, Hunk, PatchSet
 from unidiff.patch import Line
 
-from api import GIT_COMMENT_URL
+from flake8.api import legacy as flake8
+
+from api import GIT_COMMENT_URL, GIT_COMMIT_URL, GIT_COMPARE_URL, \
+    GIT_PULL_REVIEW_URL
 
 host_api = os.environ.get("GITHOST_API", "api.github.com")
 host = os.environ.get("GITHOST", "github.com")
@@ -16,7 +20,56 @@ auth = HTTPBasicAuth(os.environ.get("GITUSERNAME", "ERROR"),
 
 
 def format_comment(filename, line_n, offset, code, text):
+    """Returns formatted comment.
+     Just a dummy function to pass tuple of arguments by *args"""
     return f'{code}: {text}'
+
+
+def get_commit_parent(commit_sha: str, owner: str, repo: str):
+    """Returns parent commit sha"""
+    link = GIT_COMMIT_URL.format(sha=commit_sha,
+                                 owner=owner,
+                                 repo=repo,
+                                 host=host_api)
+    commit_info_req = get(link, auth=auth)
+    commit_info = commit_info_req.json()
+    return commit_info['parents'][0]['sha']
+
+
+def flake8_scan_file(commit_sha, owner, repo, parent_sha=None):
+    """Runs flake8 scan on all changed files and returns array of warnings"""
+    if parent_sha is None:
+        parent_sha = get_commit_parent(commit_sha, owner, repo)
+    diff_url = GIT_COMPARE_URL.format(base=parent_sha,
+                                      head=commit_sha,
+                                      owner=owner,
+                                      repo=repo,
+                                      host=host_api)
+    diff_info = get(diff_url, auth=auth).json()
+    diff_content = get(diff_url,
+                       auth=auth,
+                       headers={"Accept": "application/vnd.github.v3.diff"}
+                       ).content.decode('utf8')
+    patch_set = PatchSet(diff_content)
+    comments_per_file = {}
+    for file in diff_info['files']:
+        content = get(file['contents_url'], auth=auth).json()
+        file_content = get(content['download_url']).content
+        with open("flake8_tmp_file.py", 'wb') as test_file:
+            test_file.write(file_content)
+        style_guide = flake8.get_style_guide(ignore=['E24', 'W503'])
+        style_guide.input_file('./flake8_tmp_file.py', )
+        results = style_guide._application.file_checker_manager.checkers[
+            0].results
+        comments_per_line = {}
+        for code, line_n, offset, text, src in results:
+            if changed_in_diff(get_file_by_name(patch_set,
+                                                file['filename']), line_n):
+                comments = comments_per_line.get(line_n, [])
+                comments.append((file['filename'], line_n, offset, code, text))
+                comments_per_line[line_n] = comments
+        comments_per_file[file['filename']] = comments_per_line
+    return comments_per_file
 
 
 def changed_in_diff(diff: PatchedFile, line_n: int):
@@ -38,16 +91,35 @@ def get_file_by_name(mod_files: List[PatchedFile], filename: str):
     return None
 
 
-def post_comment_on_line(user, repo, commit_sha, line_n, filename, message):
+def post_comment_on_line(owner, repo, commit_sha, line_n, filename, message):
     """Posts commit comment on specified commit and line"""
     res = post(
-        GIT_COMMENT_URL.format(host=host_api, owner=user, repo=repo,
+        GIT_COMMENT_URL.format(host=host_api, owner=owner, repo=repo,
                                commit_sha=commit_sha),
         auth=auth, json={
-            "body": message,
-            "path": filename,
-            "position": line_n,
+            'body': message,
+            'path': filename,
+            'position': line_n,
         })
 
     assert res.status_code == 201, f'Got non 201 status, ' \
+                                   f'error message: {res.content}'
+
+
+def post_pr_review(owner, repo,
+                   commit_sha, pull_number,
+                   message, event='COMMENT'):
+    """Posts review comment on specified pull request and change it status"""
+    review = {
+        'commit_id': commit_sha,
+        'body': message,
+        'event': event,
+    }
+
+    res = post(GIT_PULL_REVIEW_URL.format(host=host_api,
+                                          owner=owner,
+                                          repo=repo,
+                                          pull_number=pull_number),
+               json=review, auth=auth)
+    assert res.status_code == 200, f'Got non 201 status, ' \
                                    f'error message: {res.content}'
